5219567b131ba19526be1fc06c4a886502c3c9d5
Tomás Navarro <tomy.brm@gmail.com>
2023-04-10 09:24:09 -0300

    Modificacion para server de produccion

--- app/routes.py
+++ app/routes.py
@@ -13,8 +13,10 @@ def index():
 @app.route('/api/prueba', methods=['POST'])
 def receive_parameters():
     request_data = request.get_json()
-    pya = request_data['pya']
-    ppm = request_data['ppm']
-    timestamp = datetime.fromtimestamp(request_data['ss_lastActivityTime'] / 1000).strftime("%d/%m/%Y - %H:%M:%S")
-    return json.dumps({'sumando': pya + ppm,
+    ppm = None
+    if request_data:
+        if 'PPM2' in request_data:
+            ppm = request_data['PPM2']
+        timestamp = datetime.fromtimestamp(request_data['ss_lastActivityTime'] / 1000).strftime("%d/%m/%Y - %H:%M:%S")
+    return json.dumps({'ajuste_ppm_prueba': ppm * 0.5,
                        'datatime': timestamp}), 200
