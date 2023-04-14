b7626ffed14823beb7e8624532b412a67f7493e8
Tomás Navarro <tomy.brm@gmail.com>
2023-04-14 11:31:51 -0300

    Formatting sql query identation

--- app/TsKvQuery.py
+++ app/TsKvQuery.py
@@ -3,6 +3,9 @@ from datetime import datetime
 
 
 class TsKvQuery:
+    """
+    Esta clase realiza consultas SQL a la base de datos de Thingsboard
+    """
 
     def __init__(self, device=None, alias=None):
         self.device = device
@@ -17,21 +20,50 @@ class TsKvQuery:
         self.db.disconnect()
 
     def get_today_accumulator(self, device):
+        """
+        Obtenemos el acumulado del día, de la tabla ts_kv_{esteAnio}_{esteMes} en base a los PPM (key 36)
+        :param device:
+        :return: acumulador diario
+        """
         result = self.db.execute_query(
-            f"SELECT SUM(long_v) FROM ts_kv_{self.date.year}_{self.month_str} t JOIN device d ON t.entity_id = d.id WHERE "
-            f"date_trunc('day', to_timestamp(ts/1000)) = '{self.date_str}' AND t.key = 36 AND d.name = '{device}'"
+            f"SELECT SUM(long_v) FROM ts_kv_{self.date.year}_{self.month_str} t JOIN device d ON t.entity_id = d.id "
+            f"WHERE date_trunc('day', to_timestamp(ts/1000)) = '{self.date_str}' AND t.key = 36 AND d.name = '{device}'"
         )
         return result
 
     def get_week_accumulator(self, device):
+        """
+        Obtenemos el acumulado de la semana, de la tabla ts_kv_{esteAnio}_{esteMes} en base a los PPM (key 36)
+        :param device:
+        :return: acumulador diario
+        """
         result = self.db.execute_query(
-            f"SELECT SUM(long_v) FROM ts_kv_{self.date.year}_{self.month_str} t JOIN device d ON t.entity_id = d.id WHERE "
-            f"date_trunc('week', to_timestamp(ts/1000)) = date_trunc('week', current_timestamp) AND t.key = 36 AND d.name = '{device}'"
+            f"SELECT SUM(long_v) FROM ts_kv_{self.date.year}_{self.month_str} t JOIN device d ON t.entity_id = d.id"
+            f" WHERE date_trunc('week', to_timestamp(ts/1000)) = date_trunc('week', current_timestamp) AND t.key = 36"
+            f" AND d.name = '{device}'"
         )
         return result
 
     def get_month_accumulator(self, device):
+        """
+        Obtenemos el acumulado del mes, de la tabla ts_kv_{esteAnio}_{esteMes} en base a los PPM (key 36)
+        :param device:
+        :return: acumulador diario
+        """
         result = self.db.execute_query(
-            f"SELECT SUM(long_v) FROM ts_kv_{self.date.year}_{self.month_str} t JOIN device d ON t.entity_id = d.id WHERE t.key = 36 AND d.name = '{device}'"
+            f"SELECT SUM(long_v) FROM ts_kv_{self.date.year}_{self.month_str} t JOIN device d ON t.entity_id = d.id"
+            f" WHERE t.key = 36 AND d.name = '{device}'"
+        )
+        return result
+
+    def get_last_values(self, device, n):
+        """
+        Obtenemos el acumulado del día, de la tabla ts_kv_{esteAnio}_{esteMes} en base a los PPM (key 36)
+        :param device:
+        :return: acumulador diario
+        """
+        result = self.db.execute_query(
+            f"SELECT SUM(long_v) FROM ts_kv_{self.date.year}_{self.month_str} t JOIN device d ON t.entity_id = d.id"
+            f"WHERE t.key = 36 AND d.name = '{device}' ORDER BY t.ts DESC LIMIT 5"
         )
         return result
