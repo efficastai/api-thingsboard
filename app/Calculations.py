68569f6501690e4beac205a2014121b8635accc2
Tomás Navarro <tomy.brm@gmail.com>
2023-04-18 12:22:32 -0300

    Test

--- app/Calculations.py
+++ app/Calculations.py
@@ -1,4 +1,5 @@
 import math
+from datetime import datetime
 
 
 class Calculations:
@@ -8,7 +9,7 @@ class Calculations:
 
     def calculate_time_values(self, pya_tuple):
         """
-        Este metodo retorna un diccionario con el total de tiempo encendido (time_on_millis) en [0] y
+        Este metodo retorna una tupla con el total de tiempo encendido (time_on_millis) en [0] y
         el total de tiempo apagado (time_off_milis) [1]
         """
         time_on_milis = 0
@@ -42,14 +43,27 @@ class Calculations:
 
         return time_on_milis, time_off_milis
 
-    def ratio_shift_time(self, pya_tuple, shift_period=None):
+    def ratio_shift_time(self, pya_tuple, shift_start=None):
         ratio_shift_time = 0
         # Retorna el tiempo encendido en [0] y el tiempo apagado en [1]
         times = self.calculate_time_values(pya_tuple)
-        # print(times)
+        timestamp_now = int(datetime.now().timestamp()) * 1000
+        shift_start = self.shift_to_timestamp(shift_start)
         if times[1] == 0:
             ratio_shift_time = 100
-        else:
+        elif shift_start is None:
             ratio_shift_time = math.floor(times[0] / (times[0] + times[1]) * 100)
+        else:
+            ratio_shift_time = math.floor(times[0] / timestamp_now - shift_start)
         # print(type(ratio_shift_time))
         return ratio_shift_time
+
+    @staticmethod
+    def shift_to_timestamp_milis(shift_start):
+        hour = int(shift_start[:2])
+        minutes = int(shift_start[3:])
+        now = datetime.now()
+        result = int(
+            datetime(now.year, now.month, now.day, hour, minutes, now.second).timestamp() * 1000)
+
+        return result
