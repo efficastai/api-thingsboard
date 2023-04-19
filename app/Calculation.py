6e53fe4dae5f8e403be4656c29d1d25451ab5e8b
Tomás Navarro <tomasnavarro.dev@gmail.com>
2023-04-19 09:41:16 -0300

    Fix

--- app/Calculation.py
+++ app/Calculation.py
@@ -24,10 +24,11 @@ class Calculation:
         time_on, time_off = self.calculate_time_values(pya_values_current_day)
 
         # Convierto el string de shift_start en un timestamp en milisegundos.
-        shift_to_timestamp_millis = self.shift_to_timestamp_milis(shift_start)
+        if shift_start is not None:
+            shift_start = self.shift_to_timestamp_milis(shift_start)
 
         # Calculo los ratios de tiempo para el turno.
-        ratio_shift_time = self.ratio_shift_time(time_on, shift_to_timestamp_millis)
+        ratio_shift_time = self.ratio_shift_time(time_on, shift_start)
 
         # Devuelvo los resultados como un diccionario JSON.
         results = {
@@ -79,7 +80,7 @@ class Calculation:
         return time_on_milis, time_off_milis
 
     @staticmethod
-    def ratio_shift_time(time_on_milis, shift_to_timestamp_milis=None):
+    def ratio_shift_time(time_on_milis, shift_start):
         """
         Metodo que calcula el ratio de encendido/apagado de una maquina en base a la hora de inicio del turno.
         En caso de que no sea especificada una hora de inicio del turno, el ratio sera calculado en base a las
@@ -90,10 +91,10 @@ class Calculation:
         timestamp_now = int(datetime.now().timestamp()) * 1000
         if time_on_milis == 0:
             ratio_shift_time = 0
-        elif shift_to_timestamp_milis is None:
+        elif shift_start is None:
             ratio_shift_time = math.floor(time_on_milis / timestamp_now * 100)
         else:
-            ratio_shift_time = math.floor(time_on_milis / (timestamp_now - shift_to_timestamp_milis) * 100)
+            ratio_shift_time = math.floor(time_on_milis / (timestamp_now - shift_start) * 100)
         # print(type(ratio_shift_time))
         return ratio_shift_time
 
@@ -105,10 +106,6 @@ class Calculation:
         @params: una hora de turno en formato string
         @return: esa hora en timestamp milisegundos en tipo de dato int
         """
-        if shift_start is None:
-            print("LLEGÓ HASTA ACÁ!")
-            return shift_start
-
         hour = int(shift_start[:2])
         minutes = int(shift_start[3:])
         now = datetime.now()
