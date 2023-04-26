93d7b15b684f13a2c2cd629e1bb1e61b618f7d84
Tomás Navarro <tomy.brm@gmail.com>
2023-04-26 10:28:37 -0300

    New connection methods

--- app/SQLiteDB.py
+++ app/SQLiteDB.py
@@ -14,6 +14,7 @@ class SQLiteDB:
     def connect(self):
         try:
             self.conn = sqlite3.connect(self.database)
+            print("Connected to the SQLite database")
         except Exception as e:
             print("Unable to connect to SQLite database")
             raise e
