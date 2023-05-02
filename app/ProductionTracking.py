c16948fa1c793612838e3149b7f1e1d0fd0255f7
Tomás Navarro <tomy.brm@gmail.com>
2023-05-02 16:51:33 -0300

    Return over 60 seconds

--- app/ProductionTracking.py
+++ app/ProductionTracking.py
@@ -18,7 +18,7 @@ class ProductionTracking:
             while not connected:
                 result = self.get_production_tracking_analysis(device, flag, target, connected)
                 time.sleep(60)
-            return result
+                return result
 
         result = self.get_production_tracking_analysis(device, flag, target, cycle_time, connected)
         return result
