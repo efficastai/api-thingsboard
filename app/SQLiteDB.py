5f38d152b60fc8bfa57916e14c0ad35934e9569b
Tomás Navarro <tomy.brm@gmail.com>
2023-05-04 11:54:06 -0300

    machines on, off, total machines

--- app/SQLiteDB.py
+++ app/SQLiteDB.py
@@ -24,7 +24,7 @@ class SQLiteDB:
             self.conn.close()
             print("Disconnected from SQLite database")
 
-    def execute_query(self, query, value=None):
+    def execute_query(self, query, value):
         if not self.conn:
             print("No connection to SQLite database")
             return None
