ad6d258ff908e2775c5695607e37f9ae59168834
Tomás Navarro <tomy.brm@gmail.com>
2023-05-05 13:00:15 -0300

    Se necesite si o si un sleep

--- app/ProductionTracking.py
+++ app/ProductionTracking.py
@@ -150,6 +150,7 @@ class ProductionTracking:
         Parámetros:
         - client: un cliente
         """
+        time.sleep(2)
         machines_on = self.query.count_machines_on(client)[0][0]
         total_machines = self.query.count_total_machines(client)[0][0]
         machines_off = total_machines - machines_on
