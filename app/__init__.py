1576fa2f1b0c9ccbb3cb8bed96c8e4b9a387ef70
Tomás Navarro <tomy.brm@gmail.com>
2023-05-09 13:42:51 -0300

    Prueba con UJSON

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
-from flask import Flask
 import ujson as json
+from flask.json import JSONEncoder
+from flask import Flask
+
+
+class CustomJSONEncoder(JSONEncoder):
+    def default(self, obj):
+        try:
+            return json.dumps(obj)
+        except TypeError:
+            return JSONEncoder.default(self, obj)
+
 
 app = Flask(__name__)
-app.json_encoder = json.JSONEncoder
-app.json_decoder = json.JSONDecoder
+app.json_encoder = CustomJSONEncoder
 
 from app import endpoints
