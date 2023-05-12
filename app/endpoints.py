ac034ae1bfeb32c7c79f304705e6c0ba0bbec8fb
Tomás Navarro <tomy.brm@gmail.com>
2023-05-12 16:00:01 -0300

    Shift validation

--- app/endpoints.py
+++ app/endpoints.py
@@ -65,6 +65,7 @@ def time_calculations():
     request_data = request.get_json()
     device_name = request_data.get('device')
     shift_start = request_data.get('shift_start')
+    shift_end = request_data.get('shift_end')
     flag = request_data.get('flag')
     time_calculation = TimeCalculation()
     machine_time_calculations = time_calculation.get_machine_time_calculations(device_name, shift_start, flag)
