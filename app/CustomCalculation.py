b76b70e40f73792feaeae209a3353b6eb2040f27
Tomás Navarro <tomy.brm@gmail.com>
2023-05-17 12:29:33 -0300

    Puentes, tabla de estaciones

--- app/CustomCalculation.py
+++ app/CustomCalculation.py
@@ -21,9 +21,7 @@ class CustomCalculation:
             pya_values = self.get_pya_values(device)
             return pya_values
         if interval is not None:
-            print("ENTRE AL INTERVALOOOOOO!!!!!")
             is_valid_value = value >= 1
-            print("ES UN VALOR VALIDO? ", is_valid_value)
             ts_int = int(ts)
             interval_to_milis = int(interval) * 60000
             print(interval_to_milis)
