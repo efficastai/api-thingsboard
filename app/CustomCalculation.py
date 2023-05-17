929050dbee5b953d7b0e4aa2a03ced243907a375
Tomás Navarro <tomy.brm@gmail.com>
2023-05-17 10:47:54 -0300

    Puentes, tabla de estaciones

--- app/CustomCalculation.py
+++ app/CustomCalculation.py
@@ -120,9 +120,7 @@ class CustomCalculation:
         Comentarios del metodo
         """
         pya_day_accumulator = self.query.get_pya_day_accumulator(device)[0][0]
-        print("ACUMULADOR DIA:", pya_day_accumulator)
-        pya_total_accumulator = self.query.get_pya_total_accumulator(device)[0][0]
-        print("ACUMULADOR TOTAL: ", pya_total_accumulator)
+        pya_total_accumulator = int(self.query.get_pya_total_accumulator(device)[0][0])
         result = {
             'api_day_accumulator': pya_day_accumulator,
             'api_total_pya_accumulator': pya_total_accumulator
